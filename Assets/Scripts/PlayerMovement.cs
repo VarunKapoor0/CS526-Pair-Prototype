fc1686f2e4e4bf6cd7859dea0a5cdeb6d681e280
varkap00r <156375930+VarunKapoor0@users.noreply.github.com>
2024-02-07 16:56:29 -0800

    Added PlayerMovement
    Added Player movement (with restriction) with materials.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,9 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public GameObject floor;
+    public Material currentFloorMaterial;
+    public Material pastFloorMaterial;
+    float horizontalMovement;
+    public float speed = 10.0f;
+    bool isCurrentTimeLine = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,30 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalMovement = Input.GetAxis("Horizontal");
+        if (horizontalMovement > 0)
+            transform.Translate(Vector3.right * speed * Time.deltaTime * horizontalMovement);
+        else
+            Debug.Log("Cannot go back!");
+
+        TimeSwitch();
     }
+
+    /*void TimeSwitch()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Debug.Log("")
+            isCurrentTimeLine = !isCurrentTimeLine;
+            if(isCurrentTimeLine == true)
+            {
+                floor.GetComponent<SpriteRenderer>().material = currentFloorMaterial;
+            }
+            else
+            {
+                floor.GetComponent<SpriteRenderer>().material = pastFloorMaterial;
+            }
+
+        }
+    }*/
 }
